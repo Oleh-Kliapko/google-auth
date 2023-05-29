e74d3a9c42dda773194fd5ab0484e43194598a80
Oleh Kliapko <goldclub1977@gmail.com>
2023-05-29 07:42:05 +0300

    fix

--- src/pages/RegistrationForm.jsx
+++ src/pages/RegistrationForm.jsx
@@ -1,18 +1,24 @@
 import React from "react";
-
-const PORT_SERVER = 5000;
+import { ReactComponent as Google } from "../google.svg";
 
 const RegistrationForm = () => {
   const handleRegistration = () => {
     // Redirect the user to the Google authentication page
-    window.location.href = `http://localhost:${PORT_SERVER}`;
+    window.location.href = `https://google-auth-bvk5.onrender.com`;
   };
 
   return (
     <div style={{ padding: "40px" }}>
       <h1>Registration Form</h1>
       <p>Please register using your Google account:</p>
-      <button onClick={handleRegistration}>Register with Google</button>
+      <button
+        onClick={handleRegistration}
+        style={{ display: "flex", textAlign: "baseline" }}
+      >
+        Register with
+        <Google style={{ paddingLeft: "2px", width: "12px", height: "auto" }} />
+        oogle
+      </button>
     </div>
   );
 };
